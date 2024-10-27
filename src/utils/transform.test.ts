36972c6f22166a92d150b0486dd1aaf4eb95e457
Calvin Tam <calvintam@gmail.com>
2024-10-27 14:04:54 -0700

    End of part 2

--- src/utils/transform.test.ts
+++ src/utils/transform.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest';
+import { expect, describe, test } from 'vitest';
 import { createTransform } from './transform';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -8,6 +8,9 @@ import { AAPL, AMZN, DIS, WMT, createNamedPosition } from '../__test__/fixtures'
 
 // You can also create custom positions using the `createNamedPosition` helper
 const GME = createNamedPosition('GME', 'GameStop Corp.');
+const AMZ = createNamedPosition('AMZ', 'AmznWithoutTheN Corp.');
+const AMOZ = createNamedPosition('AMOZ', 'AmzMissingOType Corp.');
+const OAMZ = createNamedPosition('OAMZ', 'OhhhhAmz Corp.');
 
 /* eslint-enable @typescript-eslint/no-unused-vars */
 
@@ -16,3 +19,38 @@ test('exports a createTransform function', () => {
 });
 
 // Write your tests here
+describe('tests createTransform function', () => {
+  const basePositions = [OAMZ, AAPL, AMZN, AMOZ, DIS, WMT, GME, AMZ];
+  test('not filter when empty', () => {
+    const emptyFilter = createTransform("");
+    expect(emptyFilter(basePositions)).toEqual(basePositions);
+
+    // Assuming that any string made of just spaces is also empty
+    const emptySpace = createTransform("          ");
+    expect(emptySpace(basePositions)).toEqual(basePositions);
+  });
+
+  test('filter by start of ticker', () => {
+    const startOfTicker = createTransform("Gm");
+    expect(startOfTicker(basePositions)).toEqual([GME]);
+
+    const notStartOfTicker = createTransform("apl");
+    expect(notStartOfTicker(basePositions)).toEqual([]);
+  });
+
+  test('filter by name', () => {
+    const middleOfName = createTransform("amestop");
+    expect(middleOfName(basePositions)).toEqual([GME]);
+
+    const startOfName = createTransform("apple");
+    expect(startOfName(basePositions)).toEqual([AAPL]);
+
+    const incNames = createTransform("inc");
+    expect(incNames(basePositions)).toEqual([AAPL, AMZN, WMT]);
+  });
+
+  test('sort by match', () => {
+    const amazonSortOrder = createTransform("AMZ");
+    expect(amazonSortOrder(basePositions)).toEqual([AMZ, AMZN, AMOZ, OAMZ]);
+  });
+});
--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -3,6 +3,52 @@ import type { Position } from '../types';
 export function createTransform(
   searchText: string,
 ): (positions: Array<Position>) => Array<Position> {
-  // Replace with your solution
-  return null!;
+  const searchTextLower = searchText.toLowerCase().trim();
+  const filterPosition = filterBySearchTerm(searchTextLower);
+  const sortPositions = sortBySearchTerm(searchTextLower);
+
+  return (positions: Array<Position>) => {
+    const filteredPositions = positions.filter(filterPosition);
+
+    // If nothing filtered out, just return the new array.
+    if (filteredPositions.length === positions.length) {
+      return filteredPositions;
+    }
+
+    return filteredPositions.sort(sortPositions);
+  };
+}
+
+const filterBySearchTerm = (searchTerm: string) => {
+  return (position: Position) => {
+    const ticker = position.ticker.toLowerCase();
+    const name = position.name.toLowerCase();
+    return ticker.startsWith(searchTerm) || name.includes(searchTerm);
+  };
+};
+
+const sortBySearchTerm = (searchTerm: string) => {
+  return (positionA: Position, positionB: Position) => {
+    const sortScoreA = getSortScore(positionA, searchTerm);
+    const sortScoreB = getSortScore(positionB, searchTerm);
+    if (sortScoreA > sortScoreB) {
+      return -1
+    } else if (sortScoreA < sortScoreB) {
+      return 1;
+    }
+    return 0;
+  };
+};
+
+const getSortScore = (position: Position, searchTerm: string): number => {
+  const ticker = position.ticker.toLowerCase();
+  const name = position.name.toLowerCase();
+  if (ticker === searchTerm) {
+    return 3;
+  } else if (ticker.startsWith(searchTerm)) {
+    return 2;
+  } else if (name.startsWith(searchTerm)) {
+    return 1;
+  }
+  return 0;
 }
